1a6f1473c0ca4c574db0467ac5e5376c3c7815da
FellypeGodoy <131183961+FellypeGodoy@users.noreply.github.com>
2023-09-12 19:39:12 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,37 +3,35 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Função para conectar ao banco de dados SQLite
+
 def connect_db():
     return sqlite3.connect('banco.db')
 
-# Rota para a página HTML
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Rota para buscar dados da tabela SQL
 @app.route('/buscar_dados', methods=['GET'])
 def buscar_dados():
     try:
-        # Conectar ao banco de dados
         conn = connect_db()
         cursor = conn.cursor()
 
-        # Executar consulta SQL
+
         cursor.execute("SELECT * FROM produtos")
         rows = cursor.fetchall()
 
-        # Fechar conexão com o banco de dados
+
         conn.close()
 
-        # Converter os resultados em um formato JSON
+
         data = []
         for row in rows:
             data.append({
                 'nome': row[0],
                 'valor': row[1],
-                # Adicione mais campos conforme necessário
+                # Adicionar mais campos 
             })
 
         return jsonify(data)
@@ -45,17 +43,13 @@ def buscar_dados():
 @app.route('/adicionar_dados', methods=['POST'])
 def adicionar_dados():
     try:
-        # Obter dados JSON da solicitação POST
         data = request.get_json()
 
-        # Conectar ao banco de dados
         conn = connect_db()
         cursor = conn.cursor()
 
-        # Inserir dados na tabela
         cursor.execute("INSERT INTO produtos (nome, valor) VALUES (?, ?)", (data['nome'], data['valor']))
 
-        # Salvar as alterações e fechar a conexão com o banco de dados
         conn.commit()
         conn.close()
 
